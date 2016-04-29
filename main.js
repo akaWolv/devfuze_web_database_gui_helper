e8a32604e7e4bfc31d302a915d7cdd2807881d9c
Wolv <wolvzmey@gmail.com>
2016-04-30 00:40:14 +0200

    change run confugiration + building + packing

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const config = require('./config');
-
 const electron = require('electron');
 // Module to control application life.
 const app = electron.app;
@@ -12,32 +10,30 @@ const BrowserWindow = electron.BrowserWindow;
 const ipcMain = require('electron').ipcMain;
 
 require('crash-reporter').start();
-  
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1360, height: 1200});
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 1360, height: 1200});
 
-  // and load the index.html of the app.
-  if ('development' == config.stage) {
-    mainWindow.loadURL('file://' + __dirname + '/public/index-devel.html');
-  } else {
-    mainWindow.loadURL('file://' + __dirname + '/public/index-prod.html');
-  }
+    // and load the index.html of the app.
+    mainWindow.loadURL('file://' + __dirname + '/public/index.html');
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+    // Open the DevTools.
+    if (undefined !== process.env.NODE_ENV_STAGE &&'development' == process.env.NODE_ENV_STAGE) {
+        mainWindow.webContents.openDevTools();
+    }
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 }
 
 // This method will be called when Electron has finished
@@ -46,19 +42,19 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
 
 
@@ -66,21 +62,20 @@ app.on('activate', function () {
 // IPC COMMUNICATION //
 //###################//
 
-ipcMain.on('asynchronous-message', function(event, arg) {
-  console.log(arg);  // prints "ping"
-  event.sender.send('asynchronous-reply', 'pong');
+ipcMain.on('asynchronous-message', function (event, arg) {
+    console.log(arg);  // prints "ping"
+    event.sender.send('asynchronous-reply', 'pong');
 });
 
-ipcMain.on('synchronous-message', function(event, arg) {
-  console.log(arg);  // prints "ping"
-  event.returnValue = 'pong';
+ipcMain.on('synchronous-message', function (event, arg) {
+    console.log(arg);  // prints "ping"
+    event.returnValue = 'pong';
 });
 
-ipcMain.on('import-file', function(event, args) {
-  importFile();
+ipcMain.on('import-file', function (event, args) {
+    importFile();
 });
 
 function importFile() {
-  const dialog = require('electron').dialog;
-  console.log(dialog.showOpenDialog({ properties: [ 'openFile', 'openDirectory', 'multiSelections' ]}));
+    const dialog = require('electron').dialog;
 }
